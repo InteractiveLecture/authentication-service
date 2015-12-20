bb32139260f72e42a0fac08a4118a07991c07af2
Rene Richter <Richterrettich@gmail.com>
2015-12-20 20:00:25 +0100

    Progress

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 ./gradlew clean build
 docker build -t openservice/authentication-service:latest .
 
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/authentication-service" ] ; then
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
+  docker push openservice/authentication-service:latest
+fi
